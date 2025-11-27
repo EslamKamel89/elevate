da4792b477a1da3ceffb3a4c57e06900f1ed65f7
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-27 10:20:52 +0200

    feat(api): add Product model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField(null=True , blank=True)
+    price = models.DecimalField()
+
+    def __str__(self) -> str:
+        return f"{self.name} - ${self.price}"
+
+
+
+
+
