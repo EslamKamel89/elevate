702264957cdea69e71b07843a9dbc1c260c8a550
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-27 10:23:33 +0200

    feat(api): add Product model with price precision and initial migration

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=150, unique=True)
     description = models.TextField(null=True , blank=True)
-    price = models.DecimalField()
+    price = models.DecimalField(decimal_places=6 , max_digits=8)
 
     def __str__(self) -> str:
         return f"{self.name} - ${self.price}"
