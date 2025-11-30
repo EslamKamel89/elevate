233ff31a4dd39b6330195c3514e8be2738e1bb7b
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-30 10:15:06 +0200

    feat(api): add products index endpoint and enable unauthenticated access

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns:list[URLPattern] = [
-
+    path('products' , views.product_index , name='products.index') ,
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,17 @@
-from django.http import HttpRequest
+from django import http
+from django.http import Http404, HttpRequest
 from django.shortcuts import render
+from rest_framework.decorators import api_view as api
 from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializer
 
 
-def product_list(request:HttpRequest):
-    pass
+@api(['GET'])
+def product_index(request:HttpRequest):
+    if request.method == 'GET' :
+        products = Product.objects.all()
+        serializer = ProductSerializer(products , many=True)
+        return Response(serializer.data)
+    raise Http404()
