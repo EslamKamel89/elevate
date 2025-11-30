9935c04157d7fbb437e93d1f1aa1cb0a0cb0b022
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-30 08:34:12 +0200

    feat(api): add created_at and updated_at timestamps to Product model

--- api/models.py
+++ api/models.py
@@ -5,7 +5,8 @@ class Product(models.Model):
     name = models.CharField(max_length=150, unique=True)
     description = models.TextField(null=True , blank=True)
     price = models.DecimalField(decimal_places=6 , max_digits=8)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self) -> str:
         return f"{self.name} - ${self.price}"
 
