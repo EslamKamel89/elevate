69b5cc54fecc970e152b1e1d13e72fe323041a56
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-01 08:26:14 +0200

    feat(api): refactor products endpoint to class-based view with POST support

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,9 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
     class Meta :
         model = Product
         fields = ["id" ,"name" ,"description" ,"price" ,"created_at" ,"updated_at"  ]
--- api/views.py
+++ api/views.py
@@ -1,17 +1,26 @@
 from django import http
-from django.http import Http404, HttpRequest
+from django.http import Http404
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view as api
+from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Product
 from .serializers import ProductSerializer
 
 
-@api(['GET'])
-def product_index(request:HttpRequest):
-    if request.method == 'GET' :
+class ProductView(APIView):
+    def get(self ,request:Request):
         products = Product.objects.all()
         serializer = ProductSerializer(products , many=True)
         return Response(serializer.data)
-    raise Http404()
+    def post(self , request:Request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+
+
