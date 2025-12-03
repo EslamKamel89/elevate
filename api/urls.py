26c5aab9413e6943e4d0a9df5cfcb5214e869c14
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-03 10:08:06 +0200

    feat(api): add user registration endpoint and clean up view formatting

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
-
 from django.urls import URLPattern, path
 
 from . import views
 
-urlpatterns:list[URLPattern] = [
-    path('products' , views.ProductsView.as_view() , name='products') ,
-    path('products/<int:id>' , views.ProductView.as_view() , name='product') ,
+urlpatterns: list[URLPattern] = [
+    path("products", views.ProductsView.as_view(), name="products"),
+    path("products/<int:id>", views.ProductView.as_view(), name="product"),
+    path("register", views.RegisterView.as_view(), name="register"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,19 @@
 from django import http
+from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from rest_framework import generics, status
 from rest_framework.decorators import api_view as api
+from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, RegistrationSerializer
+
+User = get_user_model()
 
 
 class ProductsView(generics.ListCreateAPIView):
@@ -18,49 +22,69 @@ class ProductsView(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
+
+
 class ProductView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'id'
+    lookup_field = "id"
+
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = RegistrationSerializer
+
+
+class RegisterView2(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ProductsView2(APIView):
-    def get(self ,request:Request):
+    def get(self, request: Request):
         products = Product.objects.all()
-        serializer = ProductSerializer(products , many=True)
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-    def post(self , request:Request):
+    def post(self, request: Request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data , status=status.HTTP_201_CREATED)
-        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class ProductView2(APIView):
-    def get(self , request:Request , id:int):
+    def get(self, request: Request, id: int):
         product = get_object_or_404(Product, id=id)
-        serializer = ProductSerializer(product , many=False)
+        serializer = ProductSerializer(product, many=False)
         return Response(serializer.data)
 
-    def put(self , request:Request , id:int):
+    def put(self, request: Request, id: int):
         product = get_object_or_404(Product, id=id)
-        serializer = ProductSerializer(instance=product,  data=request.data)
+        serializer = ProductSerializer(instance=product, data=request.data)
         if serializer.is_valid():
             with transaction.atomic():
                 serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self , request:Request, id:int):
+    def patch(self, request: Request, id: int):
         product = get_object_or_404(Product, id=id)
         serializer = ProductSerializer(product, data=request.data, partial=True)
         if serializer.is_valid():
             with transaction.atomic():
                 serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self , request:Request , id:int):
+    def delete(self, request: Request, id: int):
         product = get_object_or_404(Product, id=id)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
